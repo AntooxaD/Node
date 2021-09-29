f0821406275e1d9be72a08697037c8f05bf70f59
Anton <82974569+AntooxaD@users.noreply.github.com>
2021-09-29 19:15:59 +0300

    node

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+/**
+ * Create new file and add to file any text
+ * Read file and return text
+ */
+
+const { writeFile, readFile } = require('../file-storage.service');
+
+const fileName = 'some-file.txt';
+
+writeFile(fileName, 'this is my text');
+
+const text = readFile(fileName);
+
+console.log(text)
---
+++ js/task-2.js
@@ -0,0 +1,4 @@
+/** Create Authorization API
+ * 1. Saving creds in file
+ * 2. Add 404 error
+ */
